3a945655236da1d4ac144df25b43d13559836d6d
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-10-13 01:29:55 -0300

    split components

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,3 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default () => <h1>Portfolio</h1>;
+import Spotlight from './Spotlight';
+import Default from './Default';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #fafafa;
+`;
+
+const Container = styled.main`
+  max-width: 1170px;
+  margin: 0 auto;
+
+  a {
+    color: #000;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Portfolio = () => (
+  <Wrapper>
+    <Container>
+      <Spotlight />
+      <Default />
+      <Default />
+    </Container>
+  </Wrapper>
+);
+
+export default Portfolio;
