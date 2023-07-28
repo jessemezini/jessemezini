05fde0f258ee2b6f8d2ae3a031a600ee4385ee65
Jesse Mezini <jessemezini@gmail.com>
2023-07-28 10:56:09 -0300

    added p-5 to container

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,9 @@ export default function Contact() {
     <>
       <Nav />
 
-      <div className="max-w-6xl mx-auto">Contato</div>
+      <div className="max-w-6xl mx-auto p-5">
+        <h1 className="text-4xl mb-14 font-bold">Contato</h1>
+      </div>
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import NavLinks from "./components/navLinks";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-5 text-center">
       <NavLinks />
       <h1 className="text-7xl font-bold my-5">Jesse Mezini</h1>
       <h2 className="text-2xl text-zinc-400">Mobile Developer</h2>
