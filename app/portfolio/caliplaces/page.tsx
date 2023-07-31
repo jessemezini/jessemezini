7c8c4882779664d3312de53b11bea47949bad084
Jesse Mezini <jessemezini@gmail.com>
2023-07-31 17:12:55 -0300

    adding cali places

--- app/portfolio/caliplaces/page.tsx
+++ app/portfolio/caliplaces/page.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
+const text = "[&>p]:text-zinc-600 [&>p]:dark:text-zinc-300";
+
 export default function CaliPlaces() {
-  return <h1 className="text-4xl mb-14 font-bold">Cali Places</h1>;
+  return (
+    <div className={`text-center ${text}`}>
+      <Image
+        src="/caliplaces-dark.png"
+        width={240}
+        height={240}
+        alt="Cali Places logo"
+        className="mx-auto my-10 rounded-xl"
+      />
+
+      <h1 className="text-4xl mb-10 font-bold">App Cali Places</h1>
+      <p className={text}>
+        Aplicativo para encontrar e adicionar novos locais para praticar
+        calistenia
+      </p>
+
+      <div className="my-6">
+        <a href="https://caliplaces.com/">Website &#8594;</a>
+      </div>
+
+      <p className="my-6 text-sm italic">iOS & Android (Em desenvolvimento)</p>
+    </div>
+  );
 }
