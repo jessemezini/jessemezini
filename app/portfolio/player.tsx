35be0a9765a5ac36b11381ed29daabc1d08236f9
Jesse Mezini <jessemezini@gmail.com>
2023-08-02 14:01:19 -0300

    passing url as props

--- app/portfolio/player.tsx
+++ app/portfolio/player.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 
-export default function Player() {
+export default function Player({ url }: { url: string }) {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
@@ -13,7 +13,7 @@ export default function Player() {
     <div className="w-[390px] h-[844px] mx-auto my-10 border-4 border-zinc-800  rounded-2xl p-2">
       {isClient && (
         <ReactPlayer
-          url="https://www.youtube.com/watch?v=wayPfGvp2vw"
+          url={url}
           width="100%"
           height="100%"
           playing={true}
