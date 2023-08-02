479b46b742a2956c3786949e79cbd8d93b6f47b2
Jesse Mezini <jessemezini@gmail.com>
2023-08-02 14:20:26 -0300

    checking history.state to show home or back btn

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,17 +1,33 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import NavLinks from "./navLinks";
 
-import { BiArrowBack } from "react-icons/bi";
+import { BiArrowBack, BiHomeAlt2 } from "react-icons/bi";
 
 export const linkStyle =
   "duration-200 text-zinc-600 hover:text-zinc-500 dark:text-zinc-400 dark:hover:text-zinc-300";
 
 export default function Nav() {
+  const router = useRouter();
+
   return (
     <nav className="flex justify-between items-center h-20 max-w-7xl mx-auto px-7 mb-10">
-      <Link href="/" className={linkStyle}>
-        <BiArrowBack size={28} />
-      </Link>
+      {window.history.state ? (
+        <div
+          onClick={() => router.back()}
+          className={`${linkStyle} cursor-pointer`}
+        >
+          <BiArrowBack size={28} />
+        </div>
+      ) : (
+        <div
+          onClick={() => router.push("/")}
+          className={`${linkStyle} cursor-pointer`}
+        >
+          <BiHomeAlt2 size={28} />
+        </div>
+      )}
 
       <NavLinks />
     </nav>
