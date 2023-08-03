a77e32cff67c7da7426f3ba17a575869968935fa
Jesse Mezini <jessemezini@gmail.com>
2023-08-03 17:30:19 -0300

    added logo and info

--- app/portfolio/onibus-iracemapolis/page.tsx
+++ app/portfolio/onibus-iracemapolis/page.tsx
@@ -5,26 +5,32 @@ export default function OnibusIracemapolis() {
   return (
     <>
       <Image
-        src="/caliplaces-dark.png"
+        src="/onibusiracemapolis.png"
         width={240}
         height={240}
-        alt="Cali Places logo"
+        alt="Ônibus Iracemápolis logo"
         className="mx-auto mb-10 rounded-xl"
       />
 
       <h1 className="text-4xl mb-10 font-bold">App Ônibus Iracemápolis</h1>
       <p className="max-w-xl">
-        Aplicativo para encontrar e adicionar novos locais para praticar
-        calistenia
+        Aplicativo para usuários do transporte público de Iracemápolis/SP
       </p>
 
-      <div className="my-6">
-        <a href="https://caliplaces.com/">Website &#8594;</a>
-      </div>
-
-      <p className="my-6 text-sm italic">iOS & Android (Em desenvolvimento)</p>
-
       <Player url="https://www.youtube.com/watch?v=wayPfGvp2vw" />
+
+      <ul className="list-disc pl-5 mb-5 max-w-xl text-left">
+        <li>
+          Desenvolvido inicialmente com Ionic e Angular v1, atualmente com React
+          Native
+        </li>
+        <li>
+          Com mais de 2,4 mil downloads e classificação média de 4,7 no Google
+          Play
+        </li>
+      </ul>
+
+      <p className="max-w-xl">Stack: React Native.</p>
     </>
   );
 }
