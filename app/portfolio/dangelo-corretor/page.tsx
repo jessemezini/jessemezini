68ffbdc2b9ca690516eac52aa24eb59dfeadedb8
Jesse Mezini <jessemezini@gmail.com>
2023-08-03 17:33:08 -0300

    added logo and info

--- app/portfolio/dangelo-corretor/page.tsx
+++ app/portfolio/dangelo-corretor/page.tsx
@@ -5,7 +5,7 @@ export default function DangeloCorretor() {
   return (
     <>
       <Image
-        src="/caliplaces-dark.png"
+        src="/dangelocorretor.png"
         width={240}
         height={240}
         alt="Cali Places logo"
@@ -15,18 +15,27 @@ export default function DangeloCorretor() {
       <h1 className="text-4xl mb-10 font-bold">
         Site e Painel Admin - Dangelo Corretor
       </h1>
-      <p className="max-w-xl">
-        Aplicativo para encontrar e adicionar novos locais para praticar
-        calistenia
-      </p>
+      <p className="max-w-xl">Site do corretor de imóveis Dangelo Olivatto</p>
 
       <div className="my-6">
-        <a href="https://caliplaces.com/">Website &#8594;</a>
+        <a href="https://dangelocorretor.com/">Website &#8594;</a>
       </div>
 
-      <p className="my-6 text-sm italic">iOS & Android (Em desenvolvimento)</p>
-
       <Player url="https://www.youtube.com/watch?v=kUPHa2zzQ44" />
+
+      <ul className="list-disc pl-5 mb-5 max-w-xl text-left">
+        <li>Painel de gerenciamento desenvolvido com React e Firebase</li>
+        <li>
+          Opção drag and drop para reorganizar a galeria de fotos do imóvel
+        </li>
+        <li>Front end desenvolvido utilizando Next.js</li>
+        <li>Filtro de imóveis por tipo, cidade, bairro e busca por código</li>
+      </ul>
+
+      <p className="max-w-xl">
+        Stack: Create React app, Next.js, Material UI, React Hook Form, React
+        DnD, Firebase Typescript.
+      </p>
     </>
   );
 }
