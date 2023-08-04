d69da5ba094b958abedf81cb482c19b7c3adefcf
Jesse Mezini <jessemezini@gmail.com>
2023-08-03 22:53:15 -0300

    site cardapio ok

--- app/portfolio/site-cardapio/page.tsx
+++ app/portfolio/site-cardapio/page.tsx
@@ -5,26 +5,33 @@ export default function SiteCardapio() {
   return (
     <>
       <Image
-        src="/caliplaces-dark.png"
+        src="/sitecardapio.png"
         width={240}
         height={240}
-        alt="Cali Places logo"
+        alt="Site Cardápio logo"
         className="mx-auto mb-10 rounded-xl"
       />
 
-      <h1 className="text-4xl mb-10 font-bold">App Ônibus Iracemápolis</h1>
+      <h1 className="text-4xl mb-10 font-bold">PWA Site Cardápio</h1>
       <p className="max-w-xl">
-        Aplicativo para encontrar e adicionar novos locais para praticar
-        calistenia
+        Site cardápio para restaurantes, lanchonetes, pizzarias e etc.
       </p>
 
       <div className="my-6">
-        <a href="https://caliplaces.com/">Website &#8594;</a>
+        <a href="https://sitecardapio.netlify.app/">Website &#8594;</a>
       </div>
 
-      <p className="my-6 text-sm italic">iOS & Android (Em desenvolvimento)</p>
+      <Player url="https://www.youtube.com/watch?v=sjyoI-wdJwA" />
 
-      <Player url="https://www.youtube.com/watch?v=wayPfGvp2vw" />
+      <ul className="list-disc pl-5 mb-5 max-w-xl text-left">
+        <li>
+          Inicialmente feito em 2015 com yeoman web app generator, gulp, workbox
+          para progressive web app, servidor nginx com let&apos;s encrypt.
+        </li>
+        <li>Após 2016 atualizado para gatsbyjs e servidor netlify.</li>
+      </ul>
+
+      <p className="max-w-xl">Stack: Reactjs, gatsbyjs, styled components.</p>
     </>
   );
 }
