d4d72629869254615366d108e6c704af98be7812
Jesse Mezini <jessemezini@gmail.com>
2023-08-10 11:07:04 -0300

    added english resume

---
+++ app/resume-english/data.ts
@@ -0,0 +1,97 @@
+export const experience = [
+  {
+    startDate: "2013-01",
+    endDate: "2023",
+    title: "Web and Mobile Developer",
+    desc: "Freelancer / Solo Entrepreneur",
+    list: [
+      "Meet with clients to gather requirements, design websites and apps, and set timelines",
+      "See portfolio: https://jessemezini.vercel.app/portfolio",
+    ],
+    stack:
+      "HTML, CSS, Javascript, Typescript, React, Gatsby, Next.js, React Native, MUI, Styled Components, Tailwind, Firebase, Digital Ocean, Netlify, Vercel, Github, PWA, Wordpress, Lemp.",
+  },
+  {
+    startDate: "2014-01",
+    endDate: "2023",
+    title: "Web developer",
+    desc: "Gazeta de Iracemápolis (Freelancer)",
+    about:
+      "Gazeta de Iracemápolis is the main journalistic media company in Iracemápolis-SP",
+    list: [
+      "Website creation, development and maintenance",
+      "Web content management and social media",
+      "Creation of arts and layout of newspaper and magazine",
+    ],
+    stack: "",
+  },
+];
+
+export const education = [
+  {
+    startDate: "2013",
+    endDate: "2016",
+    title:
+      "Systems Analysis and Development, Faculdade Integrada Einstein de Limeira",
+    list: ["Programming courses, data structures and computer architecture"],
+  },
+];
+
+export const projects = [
+  {
+    startDate: "2015",
+    endDate: "2023",
+    title: "App Ônibus Iracemápolis",
+    desc: "App for users of public transport in Iracemápolis/SP",
+    list: [
+      "Initially developed with Ionic and Angular v1, currently with React Native",
+      "With over 2,400 downloads and an average rating of 4.7 on Google Play",
+    ],
+    stack: "React Native",
+  },
+  {
+    startDate: "2022",
+    endDate: "2023",
+    title: "App Medite em Casa",
+    desc: "Meditation and sleep app made in partnership with the creator of the Meditar para Despertar channel, Fabio Lima, a YouTube channel with more than 680 thousand subscribers. The app is currently not live, content is being added for release. For viewing get in touch, it's on Closed Testing from the Play Store and TestFlight for iOS.",
+    list: [
+      "Powered with React Native Expo",
+      "Management panel built with Typescript, Next.js, Material UI and Firebase",
+      "Meditations organized by categories",
+      "Songs organized by categories",
+      "Meditation of the day by appointment",
+      "Breathing exercises done with Reanimated",
+      "Social Authentication (Facebook, Google and Apple) and by email and password",
+      "Signing payments with Revenue Cat",
+    ],
+    stack:
+      "React Native, Expo, Typescript, RN Paper, Firebase, Zustand, React Hook Form, Next.js, Revenue Cat, Firebase Analytics e Crashlytics.",
+  },
+  {
+    startDate: "2023",
+    endDate: "",
+    title: "App Cali Places",
+    desc: "App to find places to practice calisthenics",
+    list: [
+      "Device location to display nearby places",
+      "Add new location by searching the city and repositioning the marker on the map",
+      "Social login with Google and Apple",
+    ],
+    stack:
+      "React Native, Expo, Typescript, NativeWind, Firebase, Firebase Emulator, React Hook Form, RN Maps, Expo Router.",
+  },
+  {
+    startDate: "2021",
+    endDate: "",
+    title: "WebSite Dangelo Corretor",
+    desc: "Realtor website Dangelo Olivatto",
+    list: [
+      "Management panel powered by React and Firebase",
+      "Drag and drop option to rearrange property photo gallery",
+      "Front end developed using Next.js",
+      "Property filter by type, city, neighborhood and search by code",
+    ],
+    stack:
+      "Create React app, Next.js, Material UI, React Hook Form, React DnD, Firebase Typescript.",
+  },
+];
